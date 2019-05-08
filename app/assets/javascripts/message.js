0e13a25022f52e6fe065a097fa09a09e94fd643b
YamamotoMao35 <nokiroram@gmail.com>
2019-05-08 17:46:02 +0900

    Update message.js
    Update message.js
    
    帰ってきたJSONをdoneメソッドで受け取り、HTMLを作成する。
    その後、作成したHTMLをメッセージ画面の一番下に追加し、その分メッセージ画面を下にスクロールするようにした。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,38 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html = `<div class="message">
+                    <div class="message-info">
+                      <div class="message-info__user">
+                        ${message.user.name}
+                      </div>
+                      <div class="message-info__date">
+                        ${message.date}
+                      </div>
+                    </div>
+                    <p class="messages__text">
+                      ${message.body}
+                    </p>
+                    <asset_path src=${message.image} >
+                  </div>`
+      return html;
+    } else {
+      var html = `<div class="message">
+                    <div class="message-info">
+                      <div class="message-info__user">
+                      ${message.user.name}
+                      </div>
+                      <div class="message-info__date">
+                      ${message.date}
+                      </div>
+                    </div>
+                    <p class="messages__text">
+                      ${message.body}
+                    </p>
+                  </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var message = new FormData(this);
@@ -10,6 +44,12 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.message')[0].sctollHeight}, 'fast');
+      $('.textbox').val('');
     })
   })
 })
