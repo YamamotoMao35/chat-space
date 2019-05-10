5aeaebc58804d8ee4df6d3fceebc39c3a7fd3f81
YamamotoMao35 <nokiroram@gmail.com>
2019-05-10 10:50:00 +0900

    Update user_search.js
    Update user_search.js
    
    　まず、追加ボタンが押された時にclickイベントが発火するようにする。
    　セレクタの第二引数にボタンのクラスを指定することで後から追加された要素に対してイベントを設定できる。
    　追加したボタンを押した時のイベントを定義した。
    親要素に対してイベントを設定しているためchat_mamber_elementという変数で.js-chat-memberの親要素を取得している。
    user_idという変数では10行目のdata-user-idを取得している。
    user_nameという変数では同様にdata-user-nameを取得している。
    チャットメンバーのところにhtmlを追加し、そのあとに親要素を削除することで再びクリックできる状態に戻す。
    　削除ボタンを押した時のイベントも定義し、チャットメンバーの欄で削除できるようにした。

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -10,7 +10,7 @@ $(function(){
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add", data-user-id="${user.id}", data-user-name="${user.name}">追加
                   </a>
                 </div>`
-      search_result.append(html);
+    search_result.append(html);
   }
 
   function appendErrMsgToHTML(msg) {
@@ -19,8 +19,28 @@ $(function(){
                     ${ msg }
                   </p>
                 </div>`
-      search_result.append(html);
+    search_result.append(html);
   }
+
+  $(document).on("click", ".chat-group-user__btn--add", function(){
+    var chat_member_element = $(".js-chat-member").parent();
+    var user_id = $(this).attr("data-user-id");
+    var user_name = $(this).attr("data-user-name");
+    var html = `<div class="chat-group-user clearfix js-chat-member", id='chat-group-user-8'>
+                  <input name="group[user_ids][]", type="hidden", value="${user_id}">
+                  <p class="chat-group-user__name">
+                    ${user_name}
+                  </p>
+                  <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除
+                  </a>
+                </div>`
+    chat_member_element.append(html);
+    $(this).parent().remove();
+  });
+
+  $(document).on("click", ".js-remove-btn", function(){
+    $(this).parent().remove();
+  });
 
   textField.on("keyup", function(){
     var input = textField.val();
@@ -34,8 +54,6 @@ $(function(){
 
     .done(function(users) {
       search_result.empty();
-      var members = $('.chat-group-user__name').text();
-      console.log(members)
       if (users.length !== 0) {
         users.forEach(function(user){
           appendUser(user);
