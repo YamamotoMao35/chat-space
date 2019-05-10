39bf4a1097209ba5a9fa732a73cc8935a50222ea
YamamotoMao35 <nokiroram@gmail.com>
2019-05-10 19:24:58 +0900

    Update messages_controller.rb
    Update messages_controller.rb
    
    indexアクションの中で送られてきたメッセージのidを取得して
    それよりあとに投稿されたメッセージをDBから取得するようにした。

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,9 @@
 class Api::MessagesController < ApplicationController
   def index
-    
+    @messages = Message.where('name LIKE(?)', "%#{"data-message_id"}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 end
