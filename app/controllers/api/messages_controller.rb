2fbb7289ec2bb50786146a0eda4e77d45307a2ae
YamamotoMao35 <nokiroram@gmail.com>
2019-05-11 16:09:27 +0900

    Update messages_controller.rb
    Update messages_controller.rb
    
    検索条件をグループごとにすることでグループごとで最新の値を取得できるようにした。

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,7 +2,7 @@ class Api::MessagesController < ApplicationController
   def index
     respond_to do |format|
       format.html
-      format.json {@messages = Message.where('id > ?', params[:id])}
+      format.json {@messages = Group.find(params[:group_id]).messages.where('id > ?', params[:id])}
     end
   end
 end
