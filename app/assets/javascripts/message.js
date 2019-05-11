f04eca640e8fc15f934f46030e80cfc84242c2e5
YamamotoMao35 <nokiroram@gmail.com>
2019-05-11 16:20:08 +0900

    Update message.js
    Update message.js
    
    定義していたbuildMessageHTMLを使わずに、先に定義していたbuildHTMLを72行目のreloadMessagesへ渡すことで行数を減らした。
    73行目で変数group_idを定義し、last_message_idについてはカスタムデータ属性の値を取得するためvalue()からdata()とした。
    また設定したgroup_idを79行目のハッシュの中に入れた。
    さらにif (messages.length != 0)とすることでメッセージが入った時だけスクロールするようにした。
    94行目でsetInterval関数を使うことで自動更新を実現した。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,65 +1,7 @@
 $(function(){
   function buildHTML(message){
-    if ( message.image.url ) {
-      var html = `<div class="message">
-                    <div class="message-info">
-                      <div class="message-info__user">
-                        ${message.user_name}
-                      </div>
-                      <div class="message-info__date">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                    <p class="message__text">
-                      ${message.body}
-                    </p>
-                    <img src="${message.image.url}" >
-                  </div>`
-      return html;
-    } else {
-      var html = `<div class="message">
-                    <div class="message-info">
-                      <div class="message-info__user">
-                        ${message.user_name}
-                      </div>
-                      <div class="message-info__date">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                    <p class="message__text">
-                      ${message.body}
-                    </p>
-                  </div>`
-      return html;
-    };
-  }
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      $('#new_message').get(0).reset();
-    })
-    .fail(function(){
-      alert('error');
-    });
-    return false;
-  });
-
-  var buildMessageHTML = function(message) {
     if ( message.body && message.image.url) {
-      var hrml = `<div class="message", data-message-id="${message.id}" >
+      var html = `<div class="message", data-message-id="${message.id}" >
                     <div class="message-info">
                       <div class="message-info__user">
                         ${message.user_name}
@@ -103,20 +45,51 @@ $(function(){
     return html;
   };
 
-  var reloadMessages = function() {
-    last_message_id = $("#data-message_id").val();
-    console.log(last_message_id)
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
     $.ajax({
-      url: '/api/messages',
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('#new_message').get(0).reset();
+    })
+    .fail(function(){
+      alert('error');
+    });
+    return false;
+  });
+
+  var reloadMessages = function() {
+    var group_id = $(".current-group").data('group_id');
+    last_message_id = $(".message:last").data('message_id');
+    $.ajax({
+      url: `/groups/${group_id}/api/messages`,
       type: 'get',
       dataType: 'json',
-      data: {id: last_message_id}
+      data: {id: last_message_id, group_id: group_id}
     })
     .done(function(messages) {
-      console.log('success');
+      if (messages.length != 0) {
+        messages.forEach(function(message){
+          var html = buildHTML(message)
+          $('.messages').append(html);
+        });
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      };
     })
     .fail(function() {
-      console.log('error');
+      alert('自動更新に失敗しました');
     });
   };
+  setInterval(reloadMessages, 5000);
 });
