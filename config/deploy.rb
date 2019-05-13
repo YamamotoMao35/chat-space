8993818e943f3b1e5b55cfb86bf89adf8af225c4
YamamotoMao35 <nokiroram@gmail.com>
2019-05-13 19:39:48 +0900

    Update deploy.rb
    Update deploy.rb
    
    公開鍵をpublickeyに変更した。

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
-set :ssh_options, auth_methods: ['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDdlTgbZ6dH/rl1YCmx4qAJy5xhDQHcOJ5d6Yj3qV2pgpx4bWyL5eABWwwQ2zyZtTtfgjlkqvwrH4ZtPZtQ4FSZycUHu+kqW3fWonxLgNaDWhu7n86lAWOaN8SKEfGTIIWmhDK57OQJZM+bDvOEgFDRlpcmshOb/mkEr1v8VvbHwHKFiZcIcQdWJCZFeElBw/brhiO3e8CKOyOdPtU7jS9i1UhPyQTynZEhM+MLB8eBTw9++RVcj2+/EJcVgqvYcxL5oYt2LWT4uxBPlTF+bkK0CWpOnyl8QZ5GUNDc0uafXRpJPiamZ/LqNXifmvShgDY81qQuwWn/qXCaORi60kifF+0mRZeOL4k2Fi1J7iXalLzgXBRH6K2BfWHomMJl6eOb4gzPC4SS+Z33j9LwQIjTZK6W8X07xZmyHSg7LmY3HuTzpaX/tmVax7mDKcJrObodqDSsMy0974ZvTsxaH0ejlLITrcty/ypiYohjrMk7sUO9VRVZtyag+hN8fLKsr3Q0d8oRG8oN2DkymX1CmgG0r/XEZpqZfwaH1D32MKR+MEh6ukhLBq3EUuKk6B4n1GosWQOa9kcR7lN/hc5fC27R0r7xUx1LWaNvTMt+j4IUbEZ6PNVSppen+U8o4//T9yu6Tyk4ME7HA5WhnQZ/njigrqv9Ax2jw+OELFEOYC/iNw=='],
+set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/.ssh/Mihouchat.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
